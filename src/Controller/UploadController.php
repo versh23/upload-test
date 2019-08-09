e5691bfd6afd1e04fb5f23e8429a48c32e2ecb00
versh23 <versh23@gmail.com>
2019-08-09 23:40:53 +0300

    upload file

---
+++ src/Controller/UploadController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use League\Flysystem\FilesystemInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class UploadController extends AbstractController
+{
+    private $storage;
+
+    public function __construct(FilesystemInterface $defaultStorage)
+    {
+        $this->storage = $defaultStorage;
+    }
+
+    /**
+     * @Route("/upload", methods={"POST"})
+     */
+    public function upload(Request $request, ValidatorInterface $validator)
+    {
+        /** @var UploadedFile $file */
+        $file = $request->files->get('file');
+
+        if (!$file) {
+            return $this->json([], 400);
+        }
+
+
+        $errors = $validator->validate($file, new File([
+            //Это например можно вынести в конфиг
+            'maxSize' => '2M',
+            'mimeTypes' => ['application/pdf', 'application/x-pdf', 'image/*']
+        ]));
+
+        if ($errors->count()) {
+            $array = [];
+            foreach ($errors as $error) {
+                $array[] = $error->getMessage();
+            }
+            return $this->json($array, 400);
+        }
+
+
+        $path = $file->getClientOriginalName();
+
+
+        $stream = fopen($file->getPathname(), 'r+b');
+        $this->storage->putStream($path, $stream, [
+            'mimetype' => $file->getMimeType(),
+        ]);
+
+        if (is_resource($stream)) {
+            fclose($stream);
+        }
+
+        $fileEntity = new \App\Entity\File();
+        $fileEntity->setPath($path);
+        $this->getDoctrine()->getManager()->persist($fileEntity);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->json($fileEntity, 200);
+    }
+}
--- src/Entity/File.php
+++ src/Entity/File.php
@@ -16,8 +16,25 @@ class File
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $path;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
     }
 }
